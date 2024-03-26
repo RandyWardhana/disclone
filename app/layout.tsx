050231c30a8f686b361990f5e88c25691ab4a0d1
Randy Wardhana <randy.wardhana@meditap.id>
2024-03-26 14:14:15 +0700

    Chat Message Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils"
 import { ModalProvider } from "@/components/providers/modal-providers"
 import { ThemeProvider } from "@/components/providers/theme-provider"
 import { SocketProvider } from "@/components/providers/socket-providers"
+import { QueryProvider } from "@/components/providers/query-providers"
 
 const font = Open_Sans({ subsets: ["latin"] })
 
@@ -37,7 +38,9 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>
+                {children}
+              </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
