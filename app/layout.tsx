2dff9df961e6e9a21725347f2de574a5695b9a5b
Randy Wardhana <randy.wardhana@meditap.id>
2024-03-28 14:37:36 +0700

    Test commit with SSH

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css"
-import type { Metadata } from "next"
 import { Open_Sans } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
+import type { Metadata } from "next"
 
 import { cn } from "@/lib/utils"
 
